ba520a4a98bd152727c48452a150bb8e6050534a
Nguyen Van Bach <vanbach1213@gmail.com>
2018-11-05 17:30:26 +0900

    Increase version to 0.3.0

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from logging import getLogger
 from core.ec2 import Ec2
 from core.volume import Volume
 
-CREDIT = 'ebsant 0.2.0'
+CREDIT = 'ebsant 0.3.0'
 
 def input():
     ''' Get input from command line. '''
