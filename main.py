1ac5af9d5a2f2b0c8e87893299842a003773d78d
Nguyen Van Bach <vanbach1213@gmail.com>
2018-11-07 11:05:56 +0900

    Update README.md

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from logging import getLogger
 from core.ec2 import Ec2
 from core.volume import Volume
 
-CREDIT = 'ebsant 0.4.0'
+CREDIT = 'ebsant 0.4.1'
 
 def input():
     ''' Get input from command line. '''
